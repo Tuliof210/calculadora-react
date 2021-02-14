cd75958b219aec8d41cb58b054c15dc7be543034
Tulio Ferreira <tulio.silva@enacom.com.br>
2021-02-14 16:23:17 -0300

    finish

--- src/app/Calculator.jsx
+++ src/app/Calculator.jsx
@@ -25,16 +25,82 @@ const btnList = [
   { label: '=', action: 'setOperation', operator: true },
 ];
 
+// a função reset usa essa const como base
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
+const operate = ({ x, op, y, original }) => {
+  switch (op) {
+    case '+':
+      return x + y;
+      break;
+    case '-':
+      return x - y;
+      break;
+    case '*':
+      return x * y;
+      break;
+    case '/':
+      return x / y || original;
+      break;
+    default:
+      return original;
+  }
+};
+
 export default class Calculator extends Component {
   reset() {
-    console.log('memory clear');
+    this.setState({ ...initialState });
   }
   setOperation(op) {
-    console.log('setting operation:', op);
+    if (this.state.current === 0)
+      this.setState({ operation: op, current: 1, clearDisplay: true });
+    else {
+      const equals = op === '=';
+      const currentOP = this.state['operation'];
+
+      const values = [...this.state['values']];
+      values[0] = operate({
+        x: values[0],
+        op: currentOP,
+        y: values[1],
+        original: values[0],
+      });
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : op,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
   }
   addDigit(d) {
-    console.log('adding digit:', d);
+    //checa se o display ja inclui o '.'
+    if (d === '.' && this.state['displayValue'].includes('.')) return;
+
+    const clearDisplay =
+      this.state['displayValue'] === '0' || this.state['clearDisplay'];
+    const currentValue = clearDisplay ? '' : this.state['displayValue'];
+    const displayValue = currentValue + d;
+
+    this.setState({ displayValue, clearDisplay: false });
+
+    const i = this.state['current'];
+    const newValue = parseFloat(displayValue);
+    const values = [...this.state['values']];
+    values[i] = newValue;
+    this.setState({ values });
   }
+
+  state = { ...initialState };
 
   constructor(props) {
     super(props);
@@ -47,7 +113,7 @@ export default class Calculator extends Component {
   render() {
     return (
       <div className="calculator-wrapper">
-        <Display value={200} />
+        <Display value={this.state['displayValue']} />
         {btnList.map((btn, index) => (
           <Button
             key={index}
