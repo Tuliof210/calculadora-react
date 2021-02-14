c7652ce36d677f4de25146086712e252316a4a4b
Tulio Ferreira <tulio.silva@enacom.com.br>
2021-02-14 13:04:32 -0300

    class component X functional component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+/*
+Dica: Tenha preferencia por criar components funcionais
+funcionais => mais simples
+de classe => mais complexos por terem estado, logo dificil de manter
+Use components de classe apenas quando necessario trabalhar com estados
+*/
+
+import React from 'react';
+
+// styles
+import './Button.scss';
--- src/index.jsx
+++ src/index.jsx
@@ -5,4 +5,10 @@ import Calculator from './app/Calculator';
 //styles
 import './index.scss';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <h1 className="header">Calculadora</h1>
+    <Calculator />
+  </div>,
+  document.getElementById('root')
+);
