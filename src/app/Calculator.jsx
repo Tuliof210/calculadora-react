05ec1578a878a58c03c1852f9ce4d164d62114b7
Tulio Ferreira <tulio.silva@enacom.com.br>
2021-02-14 12:41:39 -0300

    recreat assets folder

---
+++ src/app/Calculator.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment, Component } from 'react';
+// styles
+import './Calculator.scss';
+
+export default class Calculator extends Component {
+  render() {
+    return <div className="calculator-wrapper"></div>;
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+// my components
+import Calculator from './app/Calculator';
+//styles
+import './index.scss';
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
